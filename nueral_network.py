54ee111ee71980060ab070678faeadf79146e921
Austin Ward <ward.austin28@gmail.com>
2018-04-22 18:12:06 -0400

    Working with low accuracy

--- nueral_network.py
+++ nueral_network.py
@@ -8,16 +8,37 @@ np.random.seed(7)
 
 # dataset:
 # https://github.com/ga-students/sf-dat-21/blob/master/unit-projects/dataset/admissions.csv
-data = pd.read_csv("admissions.csv")
+# data = pd.read_csv("admissions.titled.csv")
 
-y_train = data['admit']
-x_train = data.drop('admit', axis=1)
+'''
+y_train = data['admit'].head(300)
+y_test = data['admit'].tail(100)
+x_train = data.drop('admit', axis=1).head(300)
+x_test = data.drop('admit', axis=1).tail(100)
+'''
+
+
+dataset = np.loadtxt("admissions.csv", delimiter=",")
+# split into input (X) and output (Y) variables
+X = dataset[:,1:4]
+Y = dataset[:,0]
+
 
 model = Sequential()
-model.add(Dense(units=64, activation='relu', input_dim=3))
-model.add(Dense(units=1, activation='softmax'))
-model.compile(loss='sparse_categorical_crossentropy',
-              optimizer='sgd',
-              metrics=['accuracy'])
+model.add(Dense(10, input_dim=3, activation='relu'))
+model.add(Dense(12, activation='relu'))
+model.add(Dense(8, activation='relu'))
+model.add(Dense(8, activation='relu'))
+model.add(Dense(1, activation='sigmoid'))
 
-model.fit(x_train, y_train, epochs=2, batch_size=16, verbose=2)
+# Compile model
+model.compile(
+        loss='binary_crossentropy', 
+        optimizer='adam', 
+        metrics=['accuracy'])
+
+# Fit the model
+model.fit(X, Y, epochs=15, batch_size=10)
+
+
+print(model.evaluate(X, Y))
