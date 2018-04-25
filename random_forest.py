43c88a9364acb409289ad9d0d315ca70ef30de68
Harvey Chen <harvc015@gmail.com>
2018-04-25 15:20:28 -0400

    Updated with new dataset

--- random_forest.py
+++ random_forest.py
@@ -2,13 +2,16 @@ import pandas
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 
-features = ["gre", "gpa", "prestige"]
-target = "admit"
-data = pandas.read_csv("admissions.titled.csv")
+features = ["ugrad_gpa", "gre_verbal", "gre_quant", "gre_writing"]
+target = "decision"
+data = pandas.read_csv("all_filtered.csv")
+
 train_features, test_features, train_target, test_target = train_test_split(data[features], data[target])
 
 clf = RandomForestClassifier()
 clf.fit(train_features, train_target)
+print(clf)
+
 predictions = clf.predict(test_features)
 actual = list(test_target)
 
@@ -17,4 +20,4 @@ for i in range(len(predictions)):
     if predictions[i] == actual[i]:
         correct += 1
 
-print(correct / 100.0) * 100, "%" # split makes 100 training examples
+print(correct / len(predictions))
