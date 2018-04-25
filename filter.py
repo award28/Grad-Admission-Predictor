ad6affb8e17278c8857e4f23e466ff67b0049b1a
Tom Moshier <me@tommoshier.com>
2018-04-25 19:06:23 -0400

    Added new filter

--- filter.py
+++ filter.py
@@ -2,20 +2,27 @@ import csv
 
 # link to data, https://github.com/deedy/gradcafe_data, add header to first line in order to work correctly 
 # rowid,uni_name,major,degree,season,decision,decision_method,decision_date,decision_timestamp,ugrad_gpa,gre_verbal,gre_quant,gre_writing,is_new_gre,gre_subject,status,post_data,post_timestamp,comments
-headers = ["rowid","uni_name","major","degree","season","decision",
+headers = ["rowid1","rowid2","uni_name","major","degree","season","decision",
            "decision_method","decision_date","decision_timestamp",
            "ugrad_gpa","gre_verbal","gre_quant","gre_writing",
            "is_new_gre","gre_subject","status","post_data",
            "post_timestamp","comments"]
 
-read = open("cs_clean.csv",'r',newline='')
-write = open("cs_filtered.csv",'w',newline='')
+read = open("all_filter_new_gre.csv",'r')
+write = open("school_filter_new_gre.csv",'w')
+name = open("university.txt", 'r')
+
+common = []
+for line in name:
+	common.append(line.strip('\n'))
+
+print common
 
 reader = csv.DictReader(read)
 writer = csv.DictWriter(write, fieldnames=headers)
 writer.writeheader()
 for row in reader:
-    if (row["decision"] == "Accepted" or row["decision"] == "Rejected") and (row["ugrad_gpa"] != '') and (row["gre_quant"] != ''):
+    if row["uni_name"] in common: 
         writer.writerow(row)
 
 read.close()
--- random_forest.py
+++ random_forest.py
@@ -2,9 +2,9 @@ import pandas
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 
-features = ["ugrad_gpa", "gre_verbal", "gre_quant", "gre_writing"]
+features = ["uni_name","ugrad_gpa", "gre_verbal", "gre_quant"]
 target = "decision"
-data = pandas.read_csv("all_filtered.csv")
+data = pandas.read_csv("school_filter_new_gre.csv")
 
 train_features, test_features, train_target, test_target = train_test_split(data[features], data[target])
 
@@ -15,9 +15,9 @@ print(clf)
 predictions = clf.predict(test_features)
 actual = list(test_target)
 
-correct = 0
+correct = 0.0
 for i in range(len(predictions)):
     if predictions[i] == actual[i]:
-        correct += 1
+        correct += 1.0
 
 print(correct / len(predictions))
