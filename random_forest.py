a7c416caf9b9ef9715c2c45f6dae28022e8ab663
Harvey Chen <harvc015@gmail.com>
2018-04-26 07:43:41 -0400

    Improved random forest to 69 percent

--- random_forest.py
+++ random_forest.py
@@ -2,15 +2,29 @@ import pandas
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 
-target = "decision"
-data = pandas.read_csv("all_filter.csv")
-column_name = list(data)
+
+# data = pandas.read_csv("all_filter_col.csv")
+# column_name = list(data)
+# features = column_name[1:-1]
+# target = column_name[0]
+# train_features, test_features, train_target, test_target = train_test_split(data[features], data[target],
+#                                                                              test_size=0.2, random_state=42)
+
+dtype = {"decision": bool, "ugrad_gpa": float, "gre_verbal": int, "gre_quant": int, "gre_writing": float}
+train = pandas.read_csv("train_one_hot.csv", dtype=dtype)
+test = pandas.read_csv("test_one_hot.csv", dtype=dtype)
+
+column_name = list(train)
 features = column_name[1:-1]
 target = column_name[0]
 
-train_features, test_features, train_target, test_target = train_test_split(data[features], data[target])
+train_features = train[features]
+train_target = train[target]
 
-clf = RandomForestClassifier()
+test_features = test[features]
+test_target = test[target]
+
+clf = RandomForestClassifier(n_estimators=145, max_features="log2", min_samples_split=20, random_state=14)
 clf.fit(train_features, train_target)
 
 predictions = clf.predict(test_features)
